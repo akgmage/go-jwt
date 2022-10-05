01e94617fd273d3b3840316705b71c3ab16d9abc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-05 21:22:29 +0530

    fix typos

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/akgmage/go-jwt
 
 go 1.18
 
+require go.mongodb.org/mongo-driver v1.10.3
+
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
@@ -16,10 +18,10 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- models/user-model.go
+++ models/user-model.go
@@ -6,16 +6,16 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 type User struct {
-	ID primitive.objectID `bson:"_id`
+	ID primitive.ObjectID `bson:"_id"`
 	First_name *string `json:"first_name" validate:"required min=2, max=100"` 
-	Last_name *string `json:"last_name" validate:"required min=2, max=100`
-	Password *string `json:"Password" validate:"required, min=6`
+	Last_name *string `json:"last_name" validate:"required min=2, max=100"`
+	Password *string `json:"Password" validate:"required, min=6"`
 	Email *string `json:"email" validate:"email, required"`
-	Phone *string `json:"token" validate:"required"`
+	Phone *string `json:"phone" validate:"required"`
 	Token *string `json:"token"`
 	User_type *string `json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
 	Refresh_token *string `json:"refresh_token"`
 	Created_at time.Time `json:"created_at"`
-	Updated_at  time.Time `json:updated_at"`
+	Updated_at  time.Time `json:"updated_at"`
 	User_id string `json:"user_id"`
 }
