5ec06d50233c585d1568c868fc3c4711ad8fe72d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-07 23:06:13 +0530

    add MatchUserTypeToUid function

--- helpers/auth_helper.go
+++ helpers/auth_helper.go
@@ -1,1 +1,17 @@
 package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user type")
+	uid := c.GetString("uid")
+	err = nil
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+}
