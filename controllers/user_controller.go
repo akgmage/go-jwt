1124dab84c0f9a81a9f597698be6401a238d41b5
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-08 22:24:39 +0530

    decode user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -13,6 +13,7 @@ import (
 	"github.com/akgmage/go-jwt/database"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -27,7 +28,7 @@ func Signup()
 
 func Login()
 
-func GetUsers()
+func GetUsers()							
 
 func GetUser() gin.HandlerFunc{
 	return func(c *gin.Context) {
@@ -38,5 +39,6 @@ func GetUser() gin.HandlerFunc{
 		}
 		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second) 
 		var user models.User
+		userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
 	}
 }
