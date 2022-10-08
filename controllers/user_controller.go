fe6ca7b9f36eecd4ef8169a6efcacc71031a2145
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-08 23:09:21 +0530

    add token and refresh token

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -54,6 +54,9 @@ func Signup() gin.HandlerFunc {
 		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.ID = primitive.NewObjectID()
 		user.User_id = user.ID.Hex()
+		token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+		user.Token  = &token
+		user.Refresh_token = &refreshToken
 	}
 }
 
