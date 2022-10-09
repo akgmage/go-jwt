2f58a29b671f5ff38b97e1b804af6bccd9cc5eb7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-09 17:30:52 +0530

    add SignedDetails struct

--- helpers/token_helper.go
+++ helpers/token_helper.go
@@ -6,10 +6,21 @@ import (
 	"log"
 	"os"
 	"time"
+	jwt "github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type SignedDetails struct {
+	Email string
+	First_name string
+	Last_name string
+	Uid string
+	User_type string
+	jwt.StandardClaims
+}
 
 
 func GenerateAllTokens() {
