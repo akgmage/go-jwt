73d492099d83abb47c50abae0fed7e005acf9a35
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-09 17:49:57 +0530

    add claims

--- helpers/token_helper.go
+++ helpers/token_helper.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"time"
+
+	"github.com/akgmage/go-jwt/database"
 	jwt "github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -22,7 +24,18 @@ type SignedDetails struct {
 	jwt.StandardClaims
 }
 
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var SECRET_KEY = os.Getenv("SECRET_KEY")
 
-func GenerateAllTokens() {
-
+func GenerateAllTokens(email string, firstName string, lastname string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email: email,
+		First_name: firstName,
+		Last_name: lastname,
+		Uid: uid,
+		User_type: userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
 }
