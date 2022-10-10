f7c4d17f89eda8692e1a30379ca9b1ad9b39c1b6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-10 22:52:21 +0530

    add updateObject and append token, refreshtoken and updated_at to it

--- helpers/token_helper.go
+++ helpers/token_helper.go
@@ -1,13 +1,16 @@
 package helpers
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/akgmage/go-jwt/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -46,4 +49,20 @@ func GenerateAllTokens(email string, firstName string, lastname string, userType
 		return
 	}
 	return token, refreshToken, err
+}
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+	var ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Second)
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.E{"token", signedToken})
+	updateObj = append(updateObj, bson.E{"refresh_token", signedRefreshToken})
+
+	Updated_at, _ : time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.E{"updated_at", Updated_at})
+	upsert := true
+	filter := bson.M{"user_id": userId}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
 }
