71c5ea4f25e1adb9fda4848bb4145f97f736df1a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-12 22:41:23 +0530

    assign token from header

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -3,5 +3,8 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 func Authenticate() gin.HandlerFunc{
-
+	return func(c *gin.Context) {
+		clientToekn := c.Request.Header.Get("token")
+		
+	}
 }
