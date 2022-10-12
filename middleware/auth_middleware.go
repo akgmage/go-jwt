6a8e08712c07327cd6f61bcd16d4547e36ddb41b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-12 22:43:47 +0530

    handle case for client token empty

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,10 +1,18 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Authenticate() gin.HandlerFunc{
 	return func(c *gin.Context) {
-		clientToekn := c.Request.Header.Get("token")
-		
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":fmt.Sprintf("No auth header provided")})
+		}
+
 	}
 }
