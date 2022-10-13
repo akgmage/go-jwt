d41c65d3c01f502fd72b00b613018dcbbdee8346
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-13 22:39:00 +0530

    fix errors

--- models/user-model.go
+++ models/user-model.go
@@ -7,10 +7,10 @@ import (
 )
 type User struct {
 	ID primitive.ObjectID `bson:"_id"`
-	First_name *string `json:"first_name" validate:"required min=2, max=100"` 
-	Last_name *string `json:"last_name" validate:"required min=2, max=100"`
-	Password *string `json:"Password" validate:"required, min=6"`
-	Email *string `json:"email" validate:"email, required"`
+	First_name *string `json:"first_name" validate:"required,min=2,max=100"` 
+	Last_name *string `json:"last_name" validate:"required,min=2,max=100"`
+	Password *string `json:"Password" validate:"required,min=6"`
+	Email *string `json:"email" validate:"email,required"`
 	Phone *string `json:"phone" validate:"required"`
 	Token *string `json:"token"`
 	User_type *string `json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
